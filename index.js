a3638728feefa3ca6a5b3d6002405d2c0ed6f0b4
kstambaugh <refinancethekraken@gmail.com>
2022-04-05 20:58:16 -0400

    started changing the page

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+let dogName = document.querySelectorAll('.dog-name')
+for(x in dogName){
+    dogName[x].style.textAlign = "left"
+}
+
+let footer1 = document.querySelector('.footer')
+footer1.style.color = "purple"
